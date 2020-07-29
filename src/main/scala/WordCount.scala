08a5d9fa7c47062f78667eb34fbe474858efa2db
Akshaya9501 <akshayagsawant@gmail.com>
2020-07-29 14:53:56 +0530

    removed irrelevant code

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -7,7 +7,7 @@ object WordCount {
 
   private def rddCreation(filePath: String): RDD[(Int, String)] = {
     val sc = new SparkContext("local", "WordCount")
-    val rdd = sc.textFile("src/resources/book.txt")
+    val rdd = sc.textFile(filePath)
     val wordMap = rdd.flatMap(x => x.split("\\W+"))
     val lowerCaseWords = wordMap.map(x => x.toLowerCase())
     val wordCounts = lowerCaseWords.map(x => (x, 1)).reduceByKey((x, y) => x + y)
